d6a1b10569cc59b38cb9813463992eeb897ab9ad
Uciel <cincodemarzo9@gmail.com>
2023-11-02 15:58:58 -0300

    first commit

---
+++ js/personajes-lanz.js
@@ -0,0 +1,163 @@
+//carousel animacion
+const carrusel = document.querySelector(".carrusel-items");
+const btnNxt = document.querySelector(".btn-sig");
+const btnAtr = document.querySelector(".btn-atr");
+
+btnNxt.addEventListener('click', () => {
+    carrusel.scrollLeft += 600;
+    let lis = document.querySelectorAll(".carrusel-item")
+    lis.forEach(x => {
+        x.classList.remove('moveIzq')
+        x.classList.remove('moveDer')
+        setTimeout(() => {
+            x.classList.add('moveDer');
+        }, 100)
+    })
+})
+btnAtr.addEventListener('click', () => {
+    carrusel.scrollLeft -= 600;
+    let lis = document.querySelectorAll(".carrusel-item")
+    lis.forEach(x => {
+        x.classList.remove('moveDer')
+        x.classList.remove('moveIzq')
+        setTimeout(() => {
+            x.classList.add('moveIzq');
+        }, 100)
+    })
+})
+
+
+//personajes animacion titulo
+let titulo = document.querySelector("#titulo-personajes");
+let tarjetapj = document.querySelectorAll(".carrusel-item");
+titulo.style.opacity = 0.1;
+let distance = 0;
+let movimiento = 30
+titulo.style.transform = `translateY(-${movimiento*2}px)`;
+
+tarjetapj.forEach(pj => {
+    pj.style.transform = `translateY(${movimiento}px)`;
+    pj.style.opacity = 0.1;
+});
+window.addEventListener("scroll", function() {
+    let windowHeight = window.innerHeight;
+    let elementTop = titulo.getBoundingClientRect().top;
+    let elementVisible = 300;
+    let distanceNow = window.scrollY;
+
+    if (elementTop < (windowHeight - elementVisible) && titulo.style.opacity < 1 && distanceNow > distance && movimiento > 0) {
+        titulo.style.opacity = titulo.style.opacity * 1.2;
+        titulo.style.transform = `translateY(-${movimiento*2}px)`;
+        tarjetapj.forEach(pj => {
+            pj.style.transform = `translateY(${movimiento}px)`;
+            pj.style.opacity = pj.style.opacity * 1.2;
+            pj.addEventListener('mouseover', (event) => {
+                pj.style.transform = 'scale(1.2)';
+            })
+            pj.addEventListener('mouseleave', (event) => {
+                pj.style.transform = 'scale(1)';
+            })
+        });
+
+        movimiento = movimiento - 2;
+    } else if (elementTop > (windowHeight - elementVisible) && distanceNow < distance && titulo.style.opacity > 0.1) {
+        titulo.style.opacity = titulo.style.opacity * 0.9;
+        titulo.style.transform = `translateY(-${movimiento*2}px)`;
+        tarjetapj.forEach(pj => {
+            pj.style.transform = `translateY(${movimiento}px)`;
+            pj.style.opacity = pj.style.opacity * 0.9;
+            pj.addEventListener('mouseover', (event) => {
+                pj.style.transform = 'scale(1.2)';
+            })
+            pj.addEventListener('mouseleave', (event) => {
+                pj.style.transform = 'scale(1)';
+            })
+        });
+
+        movimiento = movimiento + 2;
+    }
+
+    if (movimiento > 30) {
+        movimiento = 30;
+    }
+
+    distance = distanceNow;
+
+})
+
+//header scroll
+let header = document.querySelector("header");
+let logoTit = document.querySelector(".logo h3");
+let logoCont = document.querySelector(".logo");
+let search = document.querySelector(".search");
+let logoShark = document.querySelector(".imgLogo");
+let titlePara = document.querySelector(".titlePara");
+/* let burguerScroll = document.querySelector(".menu-burguer .burguer-active"); */
+window.addEventListener("scroll", function() {
+    header.classList.toggle("abajo", this.window.scrollY > 0);
+    logoTit.classList.toggle("titleScroll", this.window.scrollY > 0);
+    logoCont.classList.toggle("logoScroll", this.window.scrollY > 0);
+    search.classList.toggle("searchScroll", this.window.scrollY > 0);
+    logoShark.classList.toggle("logoImgScroll", this.window.scrollY > 0);
+    titlePara.classList.toggle("titleParaScroll", this.window.scrollY > 0);
+})
+
+
+//movimiento gollum parallax
+let contGol = document.querySelector(".parallax-2");
+let gollum = document.querySelector(".img-gollum");
+
+contGol.addEventListener('mouseover', (event) => {
+    contGol.addEventListener('mousemove', (event) => {
+        let mousePos = getMousePos(event);
+        let pos = mousePos.x;
+        /* console.log(pos); */
+        if (pos > 400) {
+            gollum.style.transform = `translateX(-${pos-300}px)`;
+        } else {
+            gollum.style.transform = `translateX(${pos}px)`;
+        }
+    })
+})
+
+contGol.addEventListener('mouseleave', (event) => {
+    gollum.style.transform = `translateX(0px)`;
+})
+
+function getMousePos(event) {
+    let ClientRect = contGol.getBoundingClientRect();
+    return { //objeto
+        x: Math.round(event.clientX - ClientRect.left), //canvas.offsetLeft
+        y: Math.round(event.clientY - ClientRect.top) //canvas.offsetTop
+    }
+}
+
+
+
+/* let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+    intervalo = setInterval(function() {
+        carrusel.scrollLeft = carrusel.scrollLeft + step;
+        if (carrusel.scrollLeft === maxScrollLeft) {
+            step = step * -1;
+        } else if (carrusel.scrollLeft === 0) {
+            step = step * -1;
+        }
+    }, 10);
+};
+
+const stop = () => {
+    clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+    stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+    start();
+});
+
+start(); */
